be01b0d8066dfaf09d5f40812b6be59633ba6f3a
stignarnia <80171209+stignarnia@users.noreply.github.com>
2026-01-08 17:18:29 +0000

    pwa support

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,30 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
     plugins: [
         tailwindcss(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            manifest: {
+                name: 'Match Census Transit',
+                short_name: 'CensusTransit',
+                theme_color: '#00bc7dff',
+                icons: [
+                    {
+                        src: '/favicon.png',
+                        sizes: '192x192',
+                        type: 'image/png'
+                    },
+                    {
+                        src: '/favicon.png',
+                        sizes: '512x512',
+                        type: 'image/png'
+                    }
+                ]
+            }
+        })
     ],
     build: {
         chunkSizeWarningLimit: 2000,
